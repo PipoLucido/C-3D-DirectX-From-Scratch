e156991c670143d77d3614729e2d00a798631fa3
RodrigoGabriel <monntedeoca@hotmail.com>
2020-04-12 19:26:48 -0300

    Week N01 -Window creation

---
+++ directx_Scafld/WinMaincpp.cpp
@@ -0,0 +1,50 @@
+
+#include <Windows.h>
+
+int CALLBACK WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpCmdLine,
+    int       nShowCmd
+) {
+
+    const auto pClassName = "hw3dbutts";
+    const auto windowTitle = L"Titulo window test";
+    
+
+
+    //register windows class
+    WNDCLASSEXW wc = { 0 };
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = DefWindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = nullptr;
+    wc.hCursor = nullptr;
+    wc.hbrBackground = nullptr;
+    wc.lpszMenuName = nullptr;
+    wc.lpszClassName = (LPCWSTR)pClassName;
+    wc.hIconSm = nullptr;
+
+    RegisterClassEx(&wc);
+
+
+    //create windows instance
+    HWND hwnd = CreateWindowEx(
+        0,
+        (LPCWSTR)pClassName,
+        (LPCWSTR)windowTitle,
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        200, 200,
+        640, 480,
+        nullptr,
+        nullptr,
+        hInstance,
+        nullptr
+    );
+     ShowWindow(hwnd, SW_SHOW);
+    while (true);
+    return 0;
+}
