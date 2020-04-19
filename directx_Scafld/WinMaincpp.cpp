d67bd29cfe726c204bd5879f104c4dcb8e9cd2eb
RodrigoGabriel <monntedeoca@hotmail.com>
2020-04-19 18:20:06 -0300

    Configuracion de ventana principal

--- directx_Scafld/WinMaincpp.cpp
+++ directx_Scafld/WinMaincpp.cpp
@@ -1,6 +1,20 @@
 
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hwnd,UINT msg, WPARAM wParam,LPARAM lParam ) {
+
+
+    switch (msg) {
+    case WM_CLOSE:
+            PostQuitMessage(69);
+            break;
+    }
+
+
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
@@ -17,7 +31,7 @@ int CALLBACK WinMain(
     WNDCLASSEXW wc = { 0 };
     wc.cbSize = sizeof(wc);
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -44,7 +58,22 @@ int CALLBACK WinMain(
         hInstance,
         nullptr
     );
+    //show window
      ShowWindow(hwnd, SW_SHOW);
-    while (true);
-    return 0;
+     //message pump
+     MSG msg;
+     BOOL gResult;
+     while ( (gResult = GetMessage(&msg,nullptr,0,0)) > 0)
+     {
+         TranslateMessage(&msg);
+         DispatchMessage(&msg);
+     };
+
+     if (gResult == -1)
+     {
+         return -1;
+     }
+     else {
+         return msg.wParam;
+     }
 }
